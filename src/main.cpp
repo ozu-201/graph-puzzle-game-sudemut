308e551f5ba92c2a74e22367124cb7d5d5b7d246
sm024527 <sudemut.fethiye@hotmail.com>
2023-12-21 14:13:31 +0300

    committed

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by sm024527 on 12/21/2023.
+//
+// Your First C++ Program
+
+#include <iostream>
+#include "List/Graph/Graph.h"
+
+int main() {
+    list::Graph gr{3};
+    std::cout << "Hello World!";
+    return 0;
+}
